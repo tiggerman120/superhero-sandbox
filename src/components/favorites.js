d5a0d775c59d4664519fefee71d8cceb985ee808
Garrett Cintron <grrttcintron@gmail.com>
2021-01-28 11:03:37 -0800

    initial files commit

---
+++ src/components/favorites.js
@@ -0,0 +1,5 @@
+
+
+function Favorites(props) {
+  
+}
---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/drawer';
+
+
+function Main(props) {
+  
+  
+  function clickhandler() {
+  
+  }
+  return (
+    null
+  )
+}
+
+export default Main;
